b612eaa3aa8a0f5875a0e106bb2f19046656de9e
daniellef <daniellefarias813@gmail.com>
2016-04-26 09:15:28 -0700

    login

---
+++ public_html/js/TodoList.js
@@ -0,0 +1,35 @@
+$(function (){
+    var APPLICATION_ID = "566EF471-C424-2050-FFF5-C1E17D7CC200",
+        SECRET_KEY = "2F1914AA-5A1D-D9CF-FF6D-0E13BC974900",
+        VERSION = "v1";
+        
+        Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+
+        
+    var postsCollection = Backendless.Persistence.of(Posts).find();
+        
+        console.log(postsCollection);
+        
+        var wrapper = {
+            posts: postsCollection.data
+        };
+        
+         Handlebars.registerHelper("format", function (time) {
+        return moment(time).format("dddd, MMMM Do YYYY");
+    });
+        
+        var blogScript = $("#blogs-template").html();
+        var blogTemplate = Handlebars.compile(blogScript);
+        var blogHTML = blogTemplate(wrapper);
+        
+        $('.main-container').html(blogHTML);
+});
+        
+  
+
+    function Posts(args) {
+        args = args || {};
+        this.title = args.title || "";
+        this.content = args.content || "";
+        this.authorEmail = args.authorEmail || "";
+    }
